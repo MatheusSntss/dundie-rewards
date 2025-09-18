3aec19206c926873ee0aa5cf85dfe39c2e62a4d2
Matheus <114450845+MatheusSntss@users.noreply.github.com>
2025-09-18 14:58:20 -0300

    added gitignore (#11)
    closes #8

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description = "Rewards point system for Dundie Mifflin",
+    author = "Matheus Santos",
+    packages = find_packages(),
+)
