e8bbc2b4d470fadbec20d45cc5976067d1af8fa6
MatheusSntss <matheus.snts.pvh@gmail.com>
2025-09-22 17:23:03 +0000

    Importando o argparse para pegar argumentos CLI

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,3 +1,35 @@
+import argparse
+
+def load(filepath):
+    """Loads datas from filepath to the database """
+    try:
+        with open(filepath) as file_:
+            for line in file_:
+                print(line)
+    except FileNotFoundError as e:
+        print(f"File not found {e}")
 
 def main():
-    print("Execunting entry point")
+    parser = argparse.ArgumentParser(
+        description = "Dunder Mifflin Rewards CLI",
+        epilog= "Enjoy and use with cautions",
+    )
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help="The subcommand to run",
+        choices=("load","view","send"),
+        default="help",
+        
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="File path to load",
+        default=None,
+    )
+    args = parser.parse_args()
+    
+
+if __name__ == "__main__":
+    main()
